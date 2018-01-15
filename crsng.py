286c8d9b4f6cd64f49314dd5f26da6ed34187d96
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2018-01-15 23:08:48 +0500

    first lines, nothing special

---
+++ crsng.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#  -*- coding: utf-8 -*-
+__author__ = 'Alexei Evdokimov'
+
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import crsngDBHandler as DBHandler
+
---
+++ crsngDBHandler.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#  -*- coding: utf-8 -*-
+__author__ = 'Alexei Evdokimov'
+
+
+import sqlite3 as sql
+from datetime import datetime, date, time
+
+
+class DatabaseHandler(object):
+    def __init__(self):
+        pass
