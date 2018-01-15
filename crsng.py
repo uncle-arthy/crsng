961f9786fd0ade32ff63bf3c72f87a9d461c2c36
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2018-01-16 00:05:19 +0500

    still first base code skeleton

--- crsng.py
+++ crsng.py
@@ -6,5 +6,43 @@ __author__ = 'Alexei Evdokimov'
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-import crsngDBHandler as DBHandler
+from crsngDBHandler import DatabaseHandler
 
+
+class AppMainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        #  Init database handler
+        self.db = DatabaseHandler()
+        
+        #  Decorate main window
+        self.setObjectName("MainWindow")
+        self.resize(1280, 900)
+        self.setWindowTitle("crsng | Main")
+        
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(10)
+        self.setFont(font)
+        
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("res/Stripes_icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
+        
+    #  Service function
+    def say(self, txt="WTF?!"):
+        try:
+            print("I say " + str(txt))
+        except:
+            print("Something goes wrong...")
+        
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    
+    MainWindow = AppMainWindow()
+    
+    MainWindow.show()
+    
+    sys.exit(app.exec_())
--- crsngDBHandler.py
+++ crsngDBHandler.py
@@ -9,4 +9,14 @@ from datetime import datetime, date, time
 
 class DatabaseHandler(object):
     def __init__(self):
-        pass
+        self.con = sql.connect('MainDB.db', detect_types=sql.PARSE_DECLTYPES)
+        self.cur = self.con.cursor()
+        self.cur.execute('PRAGMA encoding="UTF-8";')
+        
+        print('Connection seems to be established...')
+        
+    def close_db(self):
+        print('Commiting changes...')
+        self.con.commit()
+        print('Closing connection with DB...')
+        self.con.close()
