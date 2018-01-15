5057e2680bd917c3eb6b326e98d5ccbf01ac7bc7
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2018-01-16 00:51:15 +0500

    still first base code skeleton

--- crsng.py
+++ crsng.py
@@ -30,6 +30,64 @@ class AppMainWindow(QtWidgets.QMainWindow):
         icon.addPixmap(QtGui.QPixmap("res/Stripes_icon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         
+        #  Organize central widget where all stuff will be
+        self.centralWidget = QtWidgets.QWidget(self)
+        self.centralWidget.setObjectName('centralWidget')
+        self.gridLayout = QtWidgets.QGridLayout(self.centralWidget)
+        self.gridLayout.setObjectName('gridLayout')
+        self.setCentralWidget(self.centralWidget)
+        
+        # Organize topmenu, menubar
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1280, 21))
+        self.menubar.setObjectName('menubar')
+        
+        self.menufile = QtWidgets.QMenu(self.menubar)
+        self.menufile.setObjectName('menufile')
+        self.menufile.setTitle('File')
+        self.menuhelp = QtWidgets.QMenu(self.menubar)
+        self.menuhelp.setObjectName('menuhelp')
+        self.menuhelp.setTitle('Help')
+        self.setMenuBar(self.menubar)
+        
+        #  Organize statusbar for future needs
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName('statusbar')
+        self.setStatusBar(self.statusbar)
+        
+        #  Organize menu items (as Actions)
+        self.actionExit = QtWidgets.QAction(self)
+        self.actionExit.setObjectName('actionExit')
+        self.actionExit.setText('Exit')
+        self.actionAbout = QtWidgets.QAction(self)
+        self.actionAbout.setObjectName("actionAbout")
+        self.actionAbout.setText("About")
+        self.actionSaveAll = QtWidgets.QAction(self)
+        self.actionSaveAll.setObjectName("actionSaveAll")
+        self.actionSaveAll.setText("Save")
+        self.actionSaveAll.setToolTip("Save current state in database")
+        self.actionSaveAll.setShortcut("Ctrl+S")
+        self.actionEditChamp = QtWidgets.QAction(self)
+        self.actionEditChamp.setObjectName("actionEditChamp")
+        self.actionEditChamp.setText("Competition settings")
+        self.actionDogsDB = QtWidgets.QAction(self)
+        self.actionDogsDB.setObjectName("actionDogsDB")
+        self.actionDogsDB.setText("Dogs database")
+
+        self.menufile.addAction(self.actionDogsDB)
+        self.menufile.addSeparator()
+        self.menufile.addAction(self.actionEditChamp)
+        self.menufile.addSeparator()
+        self.menufile.addAction(self.actionSaveAll)
+        self.menufile.addSeparator()
+        self.menufile.addAction(self.actionExit)
+        self.menuhelp.addAction(self.actionAbout)
+        self.menubar.addAction(self.menufile.menuAction())
+        self.menubar.addAction(self.menuhelp.menuAction())
+
+        self.actionDogsDB.triggered.connect(self.say)
+        
+        
     #  Service function
     def say(self, txt="WTF?!"):
         try:
